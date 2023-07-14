4b36d8cceb07bac8e93d02e1e02757a1f93ec463
Abhinav Kulkarni <abhinav@concentric.ai>
2023-07-14 07:04:05 +0000

    Updated README

---
+++ models/script.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+# List all directories
+directories=$(find . -type d)
+
+# Loop through each directory
+for dir in $directories; do
+  # Change to the directory
+  cd "$dir" || continue
+
+  # Check if there are any changes to be committed
+  if [[ $(git status --porcelain) ]]; then
+    # Add all changes
+    git add .
+
+    # Commit changes
+    git commit -m "Updated README"
+
+    # Push changes
+    git push
+  fi
+
+  # Change back to the previous directory
+  cd ..
+done
+
