9b427ebc52a66274513c9f1c20d9af3c2036b4aa
Jiaming Tang <jmtang0324@gmail.com>
2023-08-14 18:07:51 -0400

    Add compatibility with GQA & optimize multi-GPU memory allocation

--- awq/quantize/auto_scale.py
+++ awq/quantize/auto_scale.py
@@ -213,11 +213,12 @@ def auto_scale_block(module, module_kwargs,
             module2inspect=module.self_attn, kwargs=module_kwargs,
         ))
         # attn out
-        scales_list.append(_auto_get_scale(
-            prev_op=module.self_attn.v_proj,
-            layers=[module.self_attn.o_proj],
-            inp=input_feat['self_attn.o_proj'],
-        ))
+        if module.self_attn.v_proj.weight.shape == module.self_attn.o_proj.weight.shape:
+            scales_list.append(_auto_get_scale(
+                prev_op=module.self_attn.v_proj,
+                layers=[module.self_attn.o_proj],
+                inp=input_feat['self_attn.o_proj'],
+            ))
         # fc1
         scales_list.append(_auto_get_scale(
             prev_op=module.post_attention_layernorm,
